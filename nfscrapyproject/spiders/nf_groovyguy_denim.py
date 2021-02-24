dc2ed4341b73ac6e88d8d6b08e357b80db1a81e9
Anthony Doan <mythicalprogrammer@gmail.com>
2021-02-23 17:32:38 -0800

    Added groovyguy spider. The url schema is totally different from other jean cut.

---
+++ nfscrapyproject/spiders/nf_groovyguy_denim.py
@@ -0,0 +1,26 @@
+import scrapy
+import logging
+from .basespiders.TateAndYokoBaseSpider import TateAndYokoBaseSpider
+
+class NfEasyguyDenimSpider(TateAndYokoBaseSpider):
+    name = 'groovyguy_denim'
+    start_urls = [
+            'http://www.tateandyoko.com/collections/naked-famous-denim-groovy-guy?page=1',
+            'http://www.tateandyoko.com/collections/naked-famous-denim-groovy-guy?page=2',
+            'http://www.tateandyoko.com/collections/naked-famous-denim-groovy-guy?page=3',
+            'http://www.tateandyoko.com/collections/naked-famous-denim-groovy-guy?page=4',
+            'http://www.tateandyoko.com/collections/naked-famous-denim-groovy-guy?page=5',
+            'http://www.tateandyoko.com/collections/naked-famous-denim-groovy-guy?page=6',
+            'http://www.tateandyoko.com/collections/naked-famous-denim-groovy-guy?page=7',
+            'http://www.tateandyoko.com/collections/naked-famous-denim-groovy-guy?page=8',
+            'http://www.tateandyoko.com/collections/naked-famous-denim-groovy-guy?page=9',
+            'http://www.tateandyoko.com/collections/naked-famous-denim-groovy-guy?page=10',
+            'http://www.tateandyoko.com/collections/naked-famous-denim-groovy-guy?page=11',
+    ]
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        
+    def parse(self, response):
+        return super().parse_denim(response)
+    
