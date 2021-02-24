46d4c1d643a145d4a6996d50b9695fa2054864e9
Anthony Doan <mythicalprogrammer@gmail.com>
2021-02-23 17:25:40 -0800

    Added skinnyguy spider.

---
+++ nfscrapyproject/spiders/nf_skinnyguy_denim.py
@@ -0,0 +1,26 @@
+import scrapy
+import logging
+from .basespiders.TateAndYokoBaseSpider import TateAndYokoBaseSpider
+
+class NfEasyguyDenimSpider(TateAndYokoBaseSpider):
+    name = 'skinnyguy_denim'
+    start_urls = [
+            'http://www.tateandyoko.com/collections/naked-and-famous-skinny-guy?page=1',
+            'http://www.tateandyoko.com/collections/naked-and-famous-skinny-guy?page=2',
+            'http://www.tateandyoko.com/collections/naked-and-famous-skinny-guy?page=3',
+            'http://www.tateandyoko.com/collections/naked-and-famous-skinny-guy?page=4',
+            'http://www.tateandyoko.com/collections/naked-and-famous-skinny-guy?page=5',
+            'http://www.tateandyoko.com/collections/naked-and-famous-skinny-guy?page=6',
+            'http://www.tateandyoko.com/collections/naked-and-famous-skinny-guy?page=7',
+            'http://www.tateandyoko.com/collections/naked-and-famous-skinny-guy?page=8',
+            'http://www.tateandyoko.com/collections/naked-and-famous-skinny-guy?page=9',
+            'http://www.tateandyoko.com/collections/naked-and-famous-skinny-guy?page=10',
+            'http://www.tateandyoko.com/collections/naked-and-famous-skinny-guy?page=11',
+    ]
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        
+    def parse(self, response):
+        return super().parse_denim(response)
+    
